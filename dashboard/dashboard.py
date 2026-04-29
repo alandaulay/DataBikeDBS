621dc3bdc1f389aa2f4ab791edd76a67b04a197f
M. ALAN DAULAY <alandaulay4@gmail.com>
2026-04-29 13:01:53 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -18,7 +18,7 @@ st.set_page_config(
 @st.cache_data
 def load_data():
     base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, "dashboard", "main_data.csv")
+    file_path = os.path.join(base_path, "main_data.csv")
 
     df = pd.read_csv(file_path)
     df['dteday'] = pd.to_datetime(df['dteday'])
